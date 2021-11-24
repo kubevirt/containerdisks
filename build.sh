715933ef34d11270b105ee736cfbeb19ef48f6e0
Roman Mohr <rmohr@redhat.com>
2021-11-24 14:30:05 +0100

    Add a Fedora 35 containerdisk
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- build.sh
+++ build.sh
@@ -5,23 +5,30 @@ set -e -o pipefail
 unset BUILD_ONLY
 
 while getopts "b" arg; do
-	case $arg in
-	b)
-		export BUILD_ONLY=true
-		;;
-	esac
+  case $arg in
+  b)
+    export BUILD_ONLY=true
+    ;;
+  esac
 done
 
 export TAG_SUFFIX=$(date +"%y%m%d%H%M")
 
-(
-	source rhcos/interface.sh
-	SHASUM=$(containerdisks::needs_update 4.9)
-	if [ -n "${SHASUM}" ]; then
-		docker build -t quay.io/containerdisks/rhcos:4.9 -t quay.io/containerdisks/rhcos:4.9-${TAG_SUFFIX} --label "shasum=${SHASUM}" -f rhcos/4.9/Dockerfile .
-		if [ -z "${BUILD_ONLY}" ]; then
-			docker push quay.io/containerdisks/rhcos:4.9-${TAG_SUFFIX}
-			docker push quay.io/containerdisks/rhcos:4.9
-		fi
-	fi
-)
+function build() {
+  export distro=$1
+  export tag=$2
+  (
+    source ${distro}/interface.sh
+    SHASUM=$(containerdisks::needs_update ${tag})
+    if [ -n "${SHASUM}" ]; then
+      docker build -t quay.io/containerdisks/${distro}:${tag} -t quay.io/containerdisks/${distro}:${tag}-${TAG_SUFFIX} --label "shasum=${SHASUM}" -f ${distro}/${tag}/Dockerfile .
+      if [ -z "${BUILD_ONLY}" ]; then
+        docker push quay.io/containerdisks/${distro}:${tag}-${TAG_SUFFIX}
+        docker push quay.io/containerdisks/${distro}:${tag}
+      fi
+    fi
+  )
+}
+
+build rhcos 4.9
+build fedora 35
