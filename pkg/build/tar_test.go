2e32e48b129ab0f118ed45f07fc68b6e8f24ceae
Roman Mohr <rmohr@redhat.com>
2021-12-06 13:23:26 +0100

    Append a slash to tar directory entries (#10)
    CDI detects the disk directory entry as a regular file due to checking
    if the tar header points to a directory by checking if it is appended
    with a slash instead of checking the tar type.
    
    To make the images works with all all CDI versions as well as meeting
    this expectation in general (most apps append a slash), adding a slash
    to the directories here too.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ pkg/build/tar_test.go
@@ -0,0 +1,62 @@
+package build
+
+import (
+	"archive/tar"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestStreamLayer(t *testing.T) {
+	type args struct {
+		imageContent string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "should create the tar file in an expected format",
+			args: args{imageContent: "hello"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+			imageName := filepath.Join(t.TempDir(), "image")
+
+			err := os.WriteFile(imageName, []byte(tt.args.imageContent), 0777)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			imageStat, err := os.Stat(imageName)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			reader, errChan := StreamLayer(imageName)
+
+			tarReader := tar.NewReader(reader)
+
+			dir, err := tarReader.Next()
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(dir.Name).To(Equal("disk/"))
+			g.Expect(int32(dir.Typeflag)).To(Equal(tar.TypeDir))
+			g.Expect(dir.Uid).To(Equal(107))
+			g.Expect(dir.Gid).To(Equal(107))
+
+			image, err := tarReader.Next()
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(image.Name).To(Equal("disk/disk.img"))
+			g.Expect(int32(image.Typeflag)).To(Equal(tar.TypeReg))
+			g.Expect(image.Size).To(Equal(imageStat.Size()))
+			g.Expect(image.Uid).To(Equal(107))
+			g.Expect(image.Gid).To(Equal(107))
+			data, err := ioutil.ReadAll(tarReader)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(string(data)).To(Equal(tt.args.imageContent))
+
+			g.Expect(errChan).ToNot(Receive())
+		})
+	}
+}
