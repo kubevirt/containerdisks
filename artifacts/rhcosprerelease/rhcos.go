cfbf06a7015d877e39f8199a0983bf150c58df5e
Roman Mohr <rmohr@redhat.com>
2022-02-08 13:15:44 +0100

    Add develper releases of RHCOS
    Add 4.9, 4.10 and latest developer releases.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ artifacts/rhcosprerelease/rhcos.go
@@ -0,0 +1,81 @@
+package rhcosprerelease
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/containers/image/v5/pkg/compression/types"
+	"kubevirt.io/containerdisks/pkg/api"
+	"kubevirt.io/containerdisks/pkg/docs"
+	"kubevirt.io/containerdisks/pkg/hashsum"
+	"kubevirt.io/containerdisks/pkg/http"
+)
+
+type rhcos struct {
+	Version     string
+	Variant     string
+	getter      http.Getter
+	Arch        string
+	Compression string
+}
+
+var description string = `RHCOS prerelease images for KubeVirt.
+<br />
+<br />
+Visit [https://docs.openshift.com/container-platform/latest/architecture/architecture-rhcos.html) to learn more about Red Hat Enterprise Linux CoreOS.`
+
+func (r *rhcos) Metadata() *api.Metadata {
+	return &api.Metadata{
+		Name:                    "rhcos-pre-release",
+		Version:                 strings.TrimPrefix(r.Version, "latest-"),
+		Description:             description,
+		ExampleCloudInitPayload: docs.Ignition(),
+	}
+}
+
+func (r *rhcos) Inspect() (*api.ArtifactDetails, error) {
+	baseURL := fmt.Sprintf("https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/pre-release/%s/", r.Version)
+	checksumURL := baseURL + "sha256sum.txt"
+	raw, err := r.getter.GetAll(checksumURL)
+	if err != nil {
+		return nil, fmt.Errorf("error downloading the rhcos sha256sum.txt file: %v", err)
+	}
+	checksums, err := hashsum.Parse(bytes.NewReader(raw), hashsum.ChecksumFormatGNU)
+	if err != nil {
+		return nil, fmt.Errorf("error reading the sha256sum.txt file: %v", err)
+	}
+
+	var artifact *api.ArtifactDetails
+	if checksum, exists := checksums[r.Variant]; exists {
+		artifact = &api.ArtifactDetails{
+			SHA256Sum:   checksum,
+			DownloadURL: baseURL + r.Variant,
+			Compression: r.Compression,
+		}
+	} else {
+		return nil, fmt.Errorf("file %q does not exist in the sha256sum file: %v", r.Variant, err)
+	}
+
+	for variant, checksum := range checksums {
+		if variant == r.Variant {
+			continue
+		}
+
+		if checksum == artifact.SHA256Sum {
+			additionalTag := strings.TrimSuffix(strings.TrimPrefix(variant, "rhcos-"), "-x86_64-openstack.x86_64.qcow2.gz")
+			artifact.AdditionalUniqueTags = append(artifact.AdditionalUniqueTags, additionalTag)
+		}
+	}
+	return artifact, nil
+}
+
+func New(release string) *rhcos {
+	return &rhcos{
+		Version:     release,
+		Arch:        "x86_64",
+		Variant:     "rhcos-openstack.x86_64.qcow2.gz",
+		getter:      &http.HTTPGetter{},
+		Compression: types.GzipAlgorithmName,
+	}
+}
---
+++ artifacts/rhcosprerelease/rhcos_test.go
@@ -0,0 +1,82 @@
+package rhcosprerelease
+
+import (
+	"testing"
+
+	"kubevirt.io/containerdisks/pkg/api"
+	"kubevirt.io/containerdisks/pkg/docs"
+	"kubevirt.io/containerdisks/testutil"
+
+	. "github.com/onsi/gomega"
+)
+
+func Test_Inspect(t *testing.T) {
+	type fields struct {
+		releaseString string
+		mockFile      string
+	}
+	type want struct {
+		artifactDetails *api.ArtifactDetails
+		metadata        *api.Metadata
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    want
+		wantErr bool
+	}{
+		{name: "rhcos:4.9", fields: fields{
+			releaseString: "latest-4.9",
+			mockFile:      "testdata/rhcos-latest-4.9-prerelease.checksum",
+		}, want: want{
+			artifactDetails: &api.ArtifactDetails{
+				SHA256Sum:            "3466690807fb710102559ea57daac0484c59ed4d914996882d601b8bb7a7ada8",
+				DownloadURL:          "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/pre-release/latest-4.9/rhcos-openstack.x86_64.qcow2.gz",
+				Compression:          "gzip",
+				AdditionalUniqueTags: []string{"4.9.0-rc.7"},
+			},
+			metadata: &api.Metadata{
+				Name:                    "rhcos-pre-release",
+				Version:                 "4.9",
+				ExampleCloudInitPayload: docs.Ignition(),
+				Description:             description,
+			},
+		},
+			wantErr: false,
+		},
+		{name: "rhcos:latest", fields: fields{
+			releaseString: "latest",
+			mockFile:      "testdata/rhcos-latest-prerelease.checksum",
+		}, want: want{
+			artifactDetails: &api.ArtifactDetails{
+				SHA256Sum:            "f581896eee37216021bfce9ddd5e1fd8289c366ca0d1db25221c77688de85fd7",
+				DownloadURL:          "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/pre-release/latest/rhcos-openstack.x86_64.qcow2.gz",
+				Compression:          "gzip",
+				AdditionalUniqueTags: []string{"4.10.0-rc.1"},
+			},
+			metadata: &api.Metadata{
+				Name:                    "rhcos-pre-release",
+				Version:                 "latest",
+				ExampleCloudInitPayload: docs.Ignition(),
+				Description:             description,
+			},
+		},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+			c := New(tt.fields.releaseString)
+			c.getter = testutil.NewMockGetter(tt.fields.mockFile)
+			got, err := c.Inspect()
+			if tt.wantErr {
+				g.Expect(err).To(HaveOccurred())
+			} else {
+				g.Expect(err).NotTo(HaveOccurred())
+			}
+			g.Expect(got).To(Equal(tt.want.artifactDetails))
+			g.Expect(c.Metadata()).To(Equal(tt.want.metadata))
+		})
+	}
+}
