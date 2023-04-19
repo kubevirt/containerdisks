b70b8a12189df0eeb0430f2587e111bd23f5aab2
Felix Matouschek <fmatouschek@redhat.com>
2023-04-19 16:23:17 +0200

    fix: Update pipeline.sh to use cirros:6.1
    cirros:5.2 does no longer exist, use cirros:6.1 instead.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- pipeline.sh
+++ pipeline.sh
@@ -7,8 +7,8 @@ make cluster-up
 
 registry="$(./hack/kubevirtci.sh registry)"
 kubeconfig="$(./hack/kubevirtci.sh kubeconfig)"
-./bin/medius images push --force --focus=cirros:5.2 --no-fail --dry-run=false --source-registry=${registry} --insecure-skip-tls --workers 3
-./bin/medius images verify --focus=cirros:5.2 --no-fail --dry-run=false --kubeconfig=${kubeconfig} --registry="registry:5000" --insecure-skip-tls --workers 3
-./bin/medius images promote --focus=cirros:5.2 --dry-run=true --source-registry=${registry} --insecure-skip-tls --workers 3
+./bin/medius images push --force --focus=cirros:6.1 --no-fail --dry-run=false --source-registry=${registry} --insecure-skip-tls --workers 3
+./bin/medius images verify --focus=cirros:6.1 --no-fail --dry-run=false --kubeconfig=${kubeconfig} --registry="registry:5000" --insecure-skip-tls --workers 3
+./bin/medius images promote --focus=cirros:6.1 --dry-run=true --source-registry=${registry} --insecure-skip-tls --workers 3
 
 make cluster-down
