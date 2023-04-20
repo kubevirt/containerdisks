kubevirt/containerdisks

commit 84225e0ec09e1e1889b88e53e7f07698e0b26922

feat: Return error when focus did not match
This changes medius to return an error if no artifact was processed.
Previously no error was returned which allowed focussing on not present
artifacts.

The introduction of named results and the renaming of variables is
necessary to satisfy linters.

Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

cmd/medius/images/common.go
@@ -23,12 +23,13 @@ type workerResult struct {
 	Value api.ArtifactResult
 }
 
-func spawnWorkers(ctx context.Context, o *common.Options, fn func(*common.Entry) (*api.ArtifactResult, error)) (chan workerResult, error) {
+func spawnWorkers(ctx context.Context, o *common.Options,
+	fn func(*common.Entry) (*api.ArtifactResult, error)) (matched bool, resultsChan chan workerResult, err error) {
 	registry := common.NewRegistry()
 	count := len(registry)
 	errChan := make(chan error, count)
 	jobChan := make(chan *common.Entry, count)
-	resultsChan := make(chan workerResult, count)
+	resultsChan = make(chan workerResult, count)
 	defer close(resultsChan)
 
 	if o.ImagesOptions.Workers > count {
@@ -42,16 +43,16 @@ func spawnWorkers(ctx context.Context, o *common.Options, fn func(*common.Entry)
 		go func() {
 			defer wg.Done()
 			for e := range jobChan {
-				result, err := fn(e)
+				result, workerErr := fn(e)
 				if result != nil {
 					resultsChan <- workerResult{
 						Key:   e.Artifact.Metadata().Describe(),
 						Value: *result,
 					}
 				}
-				if err != nil && !errors.Is(err, context.Canceled) {
+				if workerErr != nil && !errors.Is(workerErr, context.Canceled) {
 					common.Logger(e.Artifact).Error(err)
-					errChan <- err
+					errChan <- workerErr
 				}
 				if errors.Is(ctx.Err(), context.Canceled) {
 					return
@@ -60,20 +61,22 @@ func spawnWorkers(ctx context.Context, o *common.Options, fn func(*common.Entry)
 		}()
 	}
 
-	fillJobChan(jobChan, registry, o.Focus)
+	matched = fillJobChan(jobChan, registry, o.Focus)
 	close(jobChan)
 
 	wg.Wait()
 
 	select {
-	case err := <-errChan:
-		return resultsChan, err
+	case err = <-errChan:
+		return matched, resultsChan, err
 	default:
-		return resultsChan, nil
+		return matched, resultsChan, nil
 	}
 }
 
-func fillJobChan(jobChan chan *common.Entry, registry []common.Entry, focus string) {
+func fillJobChan(jobChan chan *common.Entry, registry []common.Entry, focus string) bool {
+	focusMatched := false
+
 	for i, desc := range registry {
 		if focus == "" && desc.SkipWhenNotFocused {
 			continue
@@ -84,7 +87,10 @@ func fillJobChan(jobChan chan *common.Entry, registry []common.Entry, focus stri
 		}
 
 		jobChan <- &registry[i]
+		focusMatched = true
 	}
+
+	return focusMatched
 }
 
 func writeResultsFile(fileName string, results map[string]api.ArtifactResult) error {

cmd/medius/docs/docs.go
@@ -44,6 +44,7 @@ func NewPublishDocsCommand(options *common.Options) *cobra.Command {
 
 func run(options *common.Options) error {
 	success := true
+	focusMatched := false
 
 	quayOrg, err := getQuayOrg(options.PublishDocsOptions.Registry)
 	if err != nil {
@@ -57,6 +58,7 @@ func run(options *common.Options) error {
 			continue
 		}
 
+		focusMatched = true
 		log := common.Logger(p.Artifact)
 		name := p.Artifact.Metadata().Name
 
@@ -74,6 +76,10 @@ func run(options *common.Options) error {
 				log.Errorf("error marshaling example for for %q: %v", name, err)
 			}
 		}
+	}
+
+	if !focusMatched {
+		return fmt.Errorf("no artifact was processed, focus '%s' did not match", options.Focus)
 	}
 
 	if !success {

cmd/medius/images/promote.go
@@ -27,7 +27,7 @@ func NewPromoteImagesCommand(options *common.Options) *cobra.Command {
 				logrus.Fatal(err)
 			}
 
-			resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
+			focusMatched, resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
 				description := e.Artifact.Metadata().Describe()
 				r, ok := results[description]
 				if !ok {
@@ -55,6 +55,10 @@ func NewPromoteImagesCommand(options *common.Options) *cobra.Command {
 
 			for result := range resultsChan {
 				results[result.Key] = result.Value
+			}
+
+			if !focusMatched {
+				logrus.Fatalf("no artifact was processed, focus '%s' did not match", options.Focus)
 			}
 
 			if !options.DryRun {

cmd/medius/images/push.go
@@ -43,7 +43,7 @@ func NewPublishImagesCommand(options *common.Options) *cobra.Command {
 				options.PublishImagesOptions.TargetRegistry = options.PublishImagesOptions.SourceRegistry
 			}
 
-			resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
+			focusMatched, resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
 				errString := ""
 
 				b := buildAndPublish{
@@ -72,6 +72,10 @@ func NewPublishImagesCommand(options *common.Options) *cobra.Command {
 			results := map[string]api.ArtifactResult{}
 			for result := range resultsChan {
 				results[result.Key] = result.Value
+			}
+
+			if !focusMatched {
+				logrus.Fatalf("no artifact was processed, focus '%s' did not match", options.Focus)
 			}
 
 			if !options.DryRun {

cmd/medius/images/verify.go
@@ -51,7 +51,7 @@ func NewVerifyImagesCommand(options *common.Options) *cobra.Command {
 				logrus.Fatal(err)
 			}
 
-			resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
+			focusMatched, resultsChan, workerErr := spawnWorkers(cmd.Context(), options, func(e *common.Entry) (*api.ArtifactResult, error) {
 				description := e.Artifact.Metadata().Describe()
 				r, ok := results[description]
 				if !ok {
@@ -79,6 +79,10 @@ func NewVerifyImagesCommand(options *common.Options) *cobra.Command {
 
 			for result := range resultsChan {
 				results[result.Key] = result.Value
+			}
+
+			if !focusMatched {
+				logrus.Fatalf("no artifact was processed, focus '%s' did not match", options.Focus)
 			}
 
 			if err := writeResultsFile(options.ImagesOptions.ResultsFile, results); err != nil {

commit b07b9ef42ee9e1bd65011d9fbc53498fed564ebc

fix: Log correct error when logging worker error
This regression was introduced in
84225e0ec09e1e1889b88e53e7f07698e0b26922 when renaming variables.

Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

cmd/medius/images/common.go
@@ -51,7 +51,7 @@ func spawnWorkers(ctx context.Context, o *common.Options,
 					}
 				}
 				if workerErr != nil && !errors.Is(workerErr, context.Canceled) {
-					common.Logger(e.Artifact).Error(err)
+					common.Logger(e.Artifact).Error(workerErr)
 					errChan <- workerErr
 				}
 				if errors.Is(ctx.Err(), context.Canceled) {