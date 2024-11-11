65487776d4a8af91a686c7a987278e945cb05e18
aditi-sharma-ibm <Aditi.Sharma@ibm.com>
2024-11-11 19:38:04 +0530

    Inline constant for s390x architecture in GetImageArchitecture function
    Made the s390x architecture constant private and inlined within the function, aligning it with the other architecture mappings.
    
    Signed-off-by: Aditi Sharma <Aditi.Sharma@ibm.com>

--- pkg/architecture/arch.go
+++ pkg/architecture/arch.go
@@ -2,16 +2,16 @@ package architecture
 
 import "fmt"
 
-const ArchS390x = "s390x"
-
 func GetImageArchitecture(arch string) string {
+	const archS390x = "s390x"
+
 	switch arch {
 	case "x86_64":
 		return "amd64"
 	case "aarch64":
 		return "arm64"
-	case ArchS390x:
-		return ArchS390x
+	case archS390x:
+		return archS390x
 	default:
 		panic(fmt.Sprintf("can't map unknown architecture %s to image architecture", arch))
 	}
